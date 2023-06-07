7560094ab30d087ef5f38c7eaefd44ffb909fa04
Daniel <danieldaniel22012201@google.com>
2023-06-07 12:29:24 +0800

    not sure about the PercolationStats part

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,84 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    static boolean[][] world;
+    static int worldBreadth;
+    static WeightedQuickUnionUF UF;
+    int numOpen = 0;
+    static int topVirtual1;
+    static int topVirtual2;
 
+    public Percolation(int N) {     // create N-by-N grid, with all sites initially blocked
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException("N must be larger than 0!");
+        }
+        worldBreadth = N;
+        world = new boolean[worldBreadth][worldBreadth];
+        UF = new WeightedQuickUnionUF(worldBreadth * worldBreadth + 2);
+        topVirtual1 = worldBreadth * worldBreadth;
+        topVirtual2 = worldBreadth * worldBreadth + 1;
+    }
+    public void open(int row, int col) {      // open the site (row, col) if it is not open already
+        if (row < 0 || col < 0 || row > worldBreadth - 1 || col > worldBreadth - 1) {
+            throw new IndexOutOfBoundsException("the index should be in the range from 0 to N - 1, inclusively");
+        }
+
+        if (!world[row][col]) {
+            world[row][col] = true;
+            numOpen += 1;
+            int index = xyTo1D(row, col);
+            if (row > 0 && world[row - 1][col]) {
+                int tmpIndex = xyTo1D(row - 1, col);
+                UF.union(tmpIndex, index);
+            }
+            if (row < worldBreadth - 1 && world[row + 1][col]) {
+                int tmpIndex = xyTo1D(row + 1, col);
+                UF.union(tmpIndex, index);
+            }
+            if (col > 0 && world[row][col - 1]) {
+                int tmpIndex = xyTo1D(row, col - 1);
+                UF.union(tmpIndex, index);
+            }
+            if (col < worldBreadth - 1 && world[row][col + 1]) {
+                int tmpIndex = xyTo1D(row, col + 1);
+                UF.union(tmpIndex, index);
+            }
+        }
+
+    }
+    public boolean isOpen(int row, int col) {    // is the site (row, col) open?
+        return world[row][col];
+    }
+
+    public boolean isFull(int row, int col) {  // is the site (row, col) full?
+        isPercolateHelper();
+        int tmpIndex = xyTo1D(row, col);
+        return UF.connected(tmpIndex, topVirtual1) && isOpen(row, col);
+    }
+
+    public int numberOfOpenSites() {          // number of open sites
+       return numOpen;
+    }
+
+    public boolean percolates() {              // does the system percolate?
+        isPercolateHelper();
+        return UF.connected(topVirtual1, topVirtual2);
+    }
+//    public static void main(String[] args)   // use for unit testing (not required)
+
+    private static int xyTo1D(int x, int y) {   // convert 2d coordinates to 1d index
+        return x * worldBreadth + y;
+    }
+
+    private static void isPercolateHelper() {
+        for (int i = 0; i < worldBreadth; i += 1) {
+            int tmpIndex = xyTo1D(0, i);
+            UF.union(topVirtual1, tmpIndex);
+        }
+
+        for (int i = 0; i < worldBreadth; i += 1) {
+            int tmpIndex = xyTo1D(worldBreadth - 1, i);
+            UF.union(topVirtual2, tmpIndex);
+        }
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,89 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    int repeatingTime;
+    int worldBreadth;
+    PercolationFactory PF;
+    public PercolationStats(int N, int T, PercolationFactory pf) {   // perform T independent experiments on an N-by-N grid
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must be larger than 0");
+        }
+
+        repeatingTime = T;
+        worldBreadth = N;
+        PF = pf;
+    }
+
+    public double mean() {                                           // sample mean of percolation threshold
+        int counter = 0;
+        double[] soFarEstimates = new double[repeatingTime];    //an array of doubles used to store the estimate every time
+
+        while (counter < repeatingTime) {
+            Percolation perc = PF.make(worldBreadth);   //make a new world
+
+            while (perc.percolates()) {   //make sure open a valid site, which is blocked
+                int row = StdRandom.uniform(0, worldBreadth);
+                int col = StdRandom.uniform(0, worldBreadth);
+                while (perc.world[row][col]) {
+                    row = StdRandom.uniform(0, worldBreadth);
+                    col = StdRandom.uniform(0, worldBreadth);
+                }
+                perc.open(row, col);
+            }
+
+            double estimate = (double) perc.numOpen / worldBreadth * worldBreadth;
+            soFarEstimates[counter] = estimate;
+            counter += 1;
+        }
+
+        return StdStats.mean(soFarEstimates);
+    }
+
+    public double stddev() {                                         // sample standard deviation of percolation threshold
+        double MEAN = mean();
+        double[] prevX = stddevHelper();
+        double[] tmp = new double[repeatingTime];
+        int counter = 0;
+
+        while (counter < repeatingTime) {
+            double tmpItem = (prevX[counter] - MEAN) * (prevX[counter] - MEAN);
+            tmp[counter] = tmpItem;
+            counter += 1;
+        }
+
+        return StdStats.mean(tmp) * repeatingTime / (repeatingTime - 1);
+    }
+
+    public double confidenceLow() {                                  // low endpoint of 95% confidence interval
+        return mean() - 1.96 * stddev() / Math.sqrt(repeatingTime);
+    }
+    public double confidenceHigh() {                                 // high endpoint of 95% confidence interval
+        return mean() + 1.96 * stddev() / Math.sqrt(repeatingTime);
+    }
+    private double[] stddevHelper() {
+        int counter = 0;
+        double[] soFarEstimates = new double[repeatingTime];    //an array of doubles used to store the estimate every time
+
+        while (counter < repeatingTime) {
+            Percolation perc = PF.make(worldBreadth);   //make a new world
+
+            while (perc.percolates()) {   //make sure open a valid site, which is blocked
+                int row = StdRandom.uniform(0, worldBreadth);
+                int col = StdRandom.uniform(0, worldBreadth);
+                while (perc.world[row][col]) {
+                    row = StdRandom.uniform(0, worldBreadth);
+                    col = StdRandom.uniform(0, worldBreadth);
+                }
+                perc.open(row, col);
+            }
+
+            double estimate = (double) perc.numOpen / worldBreadth * worldBreadth;
+            soFarEstimates[counter] = estimate;
+            counter += 1;
+        }
+        return soFarEstimates;
+    }
 }
