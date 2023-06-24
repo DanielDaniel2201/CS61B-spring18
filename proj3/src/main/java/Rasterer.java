79bdfe5079bb4b2fb6c613c0c6d3e3483c94ac73
Daniel <danieldaniel22012201@google.com>
2023-06-24 21:45:38 +0800

    part 1 done

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,11 @@ import java.util.Map;
  * not draw the output correctly.
  */
 public class Rasterer {
+
+    public static final double ROOT_ULLAT = 37.892195547244356, ROOT_ULLON = -122.2998046875,
+            ROOT_LRLAT = 37.82280243352756, ROOT_LRLON = -122.2119140625;
+    /** Each tile is 256x256 pixels. */
+    public static final int TILE_SIZE = 256;
 
     public Rasterer() {
         // YOUR CODE HERE
@@ -42,11 +48,157 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+        //make the numbers in params more accessible
+        double[] arrayOfNums = convertToArray(params);
+        System.out.println(Arrays.toString(arrayOfNums));
+        double lrlon = arrayOfNums[0];
+        double ullon = arrayOfNums[1];
+        double w = arrayOfNums[2];
+        double h = arrayOfNums[3];
+        double ullat = arrayOfNums[4];
+        double lrlat = arrayOfNums[5];
+
+        //calculate the appropriate depth
+        double usrLonDPP = lonDPPCalculator(lrlon, ullon, w);
+        int depth = depthCalculation(usrLonDPP);
+        System.out.println(depth);
+        String[][] twoDStringArray = StringArray(depth, lrlon, ullon, ullat, lrlat);
+        double[] fourCoors = fourCoors(depth, lrlon, ullon, ullat, lrlat);
+
+        results.put("render_grid", twoDStringArray);
+        results.put("raster_ul_lon", fourCoors[0]);
+        results.put("raster_ul_lat", fourCoors[1]);
+        results.put("raster_lr_lon", fourCoors[2]);
+        results.put("raster_lr_lat", fourCoors[3]);
+        results.put("depth", depth);
+        results.put("query_success", true);
+
+        System.out.println(Arrays.deepToString(twoDStringArray));
+        System.out.println(results);
         return results;
+    }
+
+    private double lonDPPCalculator(double longitude1, double longitude2, double width) {
+        double diff = Math.abs(longitude1 - longitude2);
+        return diff / width;
+    }
+
+    private double[] convertToArray(Map<String, Double> params) {
+        int i = 0;
+        double[] arrayOfNumbers = new double[6];
+        for (double num: params.values()) {
+            arrayOfNumbers[i] = num;
+            i += 1;
+        }
+        return arrayOfNumbers;
+    }
+
+    private int depthCalculation(double usrLonDPP) {
+        int depth = 0;
+        double lonDPP;
+        do {
+            double factor = Math.pow(2, depth);
+            double lon1 = ROOT_LRLON / factor;
+            double lon2 = ROOT_ULLON / factor;
+            lonDPP = lonDPPCalculator(lon1, lon2, TILE_SIZE);
+            depth += 1;
+        } while (lonDPP > usrLonDPP);
+
+        if (depth > 8) {
+            return 7;
+        } else {
+            return depth - 1;
+        }
+    }
+
+    private String[][] StringArray(int depth, double lrlon, double ullon, double ullat, double lrlat) {
+
+        double factor = Math.pow(2, depth);
+        double singleTileSideX = Math.abs(ROOT_LRLON - ROOT_ULLON) / factor;
+        double singleTileSideY = Math.abs(ROOT_LRLAT - ROOT_ULLAT) / factor;
+
+        if (lrlon > ROOT_LRLON) {
+            lrlon = ROOT_LRLON - singleTileSideX;
+        }
+        if (ullon < ROOT_ULLON) {
+            ullon = ROOT_ULLON;
+        }
+        if (lrlat < ROOT_LRLAT) {
+            lrlat = ROOT_LRLAT - singleTileSideY;
+        }
+        if (ullat > ROOT_ULLAT) {
+            ullat = ROOT_ULLAT;
+        }
+
+        double mapLeftToBoxLeft = Math.abs(ullon - ROOT_ULLON);
+        double mapUpToBoxUp = Math.abs(ullat - ROOT_ULLAT);
+        double mapLeftToBoxRight = Math.abs(lrlon - ROOT_ULLON);
+        double mapUpToBoxDown = Math.abs(lrlat - ROOT_ULLAT);
+
+        int ulTileX = (int)Math.floor(mapLeftToBoxLeft / singleTileSideX);
+        int ulTileY = (int)Math.floor(mapUpToBoxUp / singleTileSideY);
+        int lrTileX = (int)Math.floor(mapLeftToBoxRight / singleTileSideX);
+        int lrTileY = (int)Math.floor(mapUpToBoxDown / singleTileSideY);
+
+        System.out.println(ulTileX);
+        System.out.println(ulTileY);
+        System.out.println(lrTileX);
+        System.out.println(lrTileY);
+
+        int stringWidth = Math.abs(lrTileX - ulTileX) + 1;
+        int stringHeight = Math.abs(ulTileY - lrTileY) + 1;
+        String[][] StringArray = new String[stringHeight][stringWidth];
+
+        for (int i = 0; i <= lrTileY - ulTileY; i += 1) {
+            for (int j = 0; j <= lrTileX - ulTileX; j += 1) {
+                int x = ulTileX + j;
+                int y = ulTileY + i;
+                StringArray[i][j] = "d" + depth + "_x" + x + "_y" + y + ".png";
+            }
+        }
+
+        return StringArray;
+    }
+
+    private double[] fourCoors(int depth, double lrlon, double ullon, double ullat, double lrlat) {
+
+        double factor = Math.pow(2, depth);
+        double singleTileSideX = Math.abs(ROOT_LRLON - ROOT_ULLON) / factor;
+        double singleTileSideY = Math.abs(ROOT_LRLAT - ROOT_ULLAT) / factor;
+
+        if (lrlon > ROOT_LRLON) {
+            lrlon = ROOT_LRLON - singleTileSideX;
+        }
+        if (ullon < ROOT_ULLON) {
+            ullon = ROOT_ULLON - singleTileSideX;
+        }
+        if (lrlat < ROOT_LRLAT) {
+            lrlat = ROOT_LRLAT - singleTileSideY;
+        }
+        if (ullat > ROOT_ULLAT) {
+            ullat = ROOT_ULLAT - singleTileSideY;
+        }
+
+        double mapLeftToBoxLeft = Math.abs(ullon - ROOT_ULLON);
+        double mapUpToBoxUp = Math.abs(ullat - ROOT_ULLAT);
+        double mapLeftToBoxRight = Math.abs(lrlon - ROOT_ULLON);
+        double mapUpToBoxDown = Math.abs(lrlat - ROOT_ULLAT);
+
+        int ulTileX = (int)Math.floor(mapLeftToBoxLeft / singleTileSideX);
+        int ulTileY = (int)Math.floor(mapUpToBoxUp / singleTileSideY);
+        int lrTileX = (int)Math.floor(mapLeftToBoxRight / singleTileSideX);
+        int lrTileY = (int)Math.floor(mapUpToBoxDown / singleTileSideY);
+
+        double[] coorsArray = new double[4];
+        coorsArray[0] = ulTileX * singleTileSideX + ROOT_ULLON;
+        coorsArray[1] = ROOT_ULLAT - ulTileY * singleTileSideY;
+        coorsArray[2] = (lrTileX  + 1) * singleTileSideX + ROOT_ULLON;
+        coorsArray[3] = ROOT_ULLAT - (lrTileY + 1) * singleTileSideY;
+
+        return coorsArray;
     }
 
 }
